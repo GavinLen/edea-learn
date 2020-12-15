7696fc3a4b681057c250f14113fe2ca6f2afd726
gavinlen <gavin_len@163.com>
2020-12-15 22:24:21 +0800

    提交代码

--- databases-learn/databases-mybatis/databases-mybatis-base/src/test/java/xyz/eden/learn/databases/mybatis/dao/UserDaoTest.java
+++ databases-learn/databases-mybatis/databases-mybatis-base/src/test/java/xyz/eden/learn/databases/mybatis/dao/UserDaoTest.java
@@ -1,11 +1,31 @@
 package xyz.eden.learn.databases.mybatis.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
 import org.junit.Test;
+import xyz.eden.learn.databases.common.entity.UserEntity;
 
 public class UserDaoTest extends BaseTest {
 
     @Test
     public void addUser() {
 
+    }
+
+    @Test
+    public void getUserById() {
+        final SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        UserEntity userEntity = userDao.getUserById(1L);
+
+        Assert.assertNotNull(userEntity);
+    }
+
+    @Test
+    public void get() {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            UserEntity userEntity = sqlSession.selectOne("xyz.eden.learn.databases.mybatis.dao.UserDao.getUserById", 1);
+            Assert.assertNotNull(userEntity);
+        }
     }
 }
